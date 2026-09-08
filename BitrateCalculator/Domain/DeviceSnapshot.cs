e73af43c99928c3547f75b97f89eafd1400c3891
Dino <dino.fazlic10@gmail.com>
2026-09-08 22:38:09 +0200

    Add domain models for parsed counter readings

---
+++ BitrateCalculator/Domain/DeviceSnapshot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitrateCalculator.Domain
+{
+    public sealed record DeviceSnapshot
+    {
+        public required string Device { get; init; }
+        public required string Model { get; init; }
+        public required IReadOnlyList<NicSample> Nics { get; init; }
+    }
+}
---
+++ BitrateCalculator/Domain/NicSample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitrateCalculator.Domain
+{
+    public sealed record NicSample
+    {
+        public required string Description { get; init; }
+        public required string Mac { get; init; }
+        public required DateTimeOffset Timestamp { get; init; }
+        public required ulong RxOctets { get; init; }
+        public required ulong TxOctets { get; init; }
+    }
+}
