6d2a3cd00be3bafd4b02429ae348b47814921fc3
Dino <dino.fazlic10@gmail.com>
2026-09-08 22:32:40 +0200

    Add API models mirroring the X-Video JSON payload

---
+++ BitrateCalculator/Api/DeviceResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BitrateCalculator.Api
+{
+    public sealed class DeviceResponse
+    {
+        public string? Device { get; set; }
+        public string? Model { get; set; }
+        [JsonPropertyName("NIC")]
+        public List<NicResponse>? Nics { get; set; } 
+    }
+}
---
+++ BitrateCalculator/Api/NicResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BitrateCalculator.Api
+{
+    public sealed class NicResponse
+    {
+        public string? Description { get; set; }
+        [JsonPropertyName("MAC")]
+        public string? Mac { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Rx { get; set; }
+        public string? Tx { get; set; }
+
+    }
+}
